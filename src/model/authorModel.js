e52eff194a743cde1b3c4ff29001f48a730f7325
ashubosh <ashubosh@gmail.com>
2022-09-05 16:52:40 +0530

    work2

--- src/model/authorModel.js
+++ src/model/authorModel.js
@@ -1,6 +1,13 @@
 const mongoose = require ('mongoose')
 const authorSchema = new mongoose.Schema(
     {
-        f
+        fname: {
+            type: String,
+            required: true
+        },
+        lname: {
+            type: String,
+            required: true
+        },
     }
 )
