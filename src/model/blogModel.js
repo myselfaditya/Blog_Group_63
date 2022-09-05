0b334917de5f953d55a617b39637cc9fd29ff5f3
mirzatouqir <mirzatouqir1995@gmail.com>
2022-09-05 16:50:08 +0530

    project

--- src/model/blogModel.js
+++ src/model/blogModel.js
@@ -1,1 +1,17 @@
 const mongoose=require('mongoose')
+const objectid=mongoose.Schema.Types.ObjectId
+
+const blogScehma=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    authorId:{
+       tyep:objectId,
+       ref:mongoose.Schema.Types.ObjectI
+    }
+})
