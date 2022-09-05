b25092a34eb5aef89c160071722a8ab47fd87097
mirzatouqir <mirzatouqir1995@gmail.com>
2022-09-05 17:46:32 +0530

    work1

--- src/model/blogModel.js
+++ src/model/blogModel.js
@@ -1,17 +1,41 @@
-const mongoose=require('mongoose')
-const objectid=mongoose.Schema.Types.ObjectId
+const mongoose = require('mongoose')
+const objectid = mongoose.Schema.Types.ObjectId
 
-const blogScehma=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const blogScehma = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    body:{
-        type:String,
-        required:true
+    body: {
+        type: String,
+        required: true
     },
-    authorId:{
-       tyep:objectId,
-       ref:mongoose.Schema.Types.ObjectI
+    authorId: {
+        tyep: objectid,
+        ref: authorSchemagit ,
+        required: true
+    },
+    tags: ["String"],
+
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: ["String"],
+
+    deletedAt: Date,
+
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    publishedAt: Date,
+
+    isPublished: {
+        type: Boolean,
+        default: false
     }
-})
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('Blog', blogScehma)
