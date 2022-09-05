f4f3238d3d727d136b14be1779357e730df68002
myselfaditya <adityasuhaspatil@gmail.com>
2022-09-05 18:34:21 +0530

    work3

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,1 +1,16 @@
-const mongoose = require("mongoose")
+
+const authorModel = require("../model/authorModel")
+
+const createAuthor = async function (req, res) {
+    try {
+        let author = req.body
+        let authorCreated = await authorModel.create(author)
+        res.status(201).send({ data: authorCreated })
+    }
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
+}
+
+module.exports.createAuthor = createAuthor
