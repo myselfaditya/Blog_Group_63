cdf694c07c1c96c5816227c8719bd49f1a852e64
salman170 <salmanansari02120@gmail.com>
2022-09-06 14:19:39 +0530

    working

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -13,14 +13,18 @@ const authorModel = require("../model/authorModel")
 const createAuthor = async function (req, res) {
     try {
         let data = req.body
+        if( Object.keys(data).length == 0 ) {
+            return res.status(400).send({ status: false, message: "data is missing in body"})
+        }
+    
         let email = data.email
         // if (!validateEmail.validate(email)) return res.status(400).send({ status: false, msg: "Enter a valid email" })
-
+    
         let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
         if (!validEmail.test(email)) {
             return res.status(400).send({ status: false, message: "please enter email in  correct format" })
         }
-
+    
         let authorCreated = await authorModel.create(data)
         res.status(201).send({ status : true , data: authorCreated })
     }
